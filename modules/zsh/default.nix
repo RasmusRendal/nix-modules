3f259ba1d9d35ca92504295d90bc191f80389367
Rasmus Rendal <rasmus@rend.al>
2026-03-22 01:19:29 +0100

    Get rid of oh-my-zsh
    My, starting a new terminal was slow

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,27 +1,101 @@
 {
+  lib,
   pkgs,
   ...
 }:
+let
+  zsh-helix-mode = pkgs.fetchFromGitHub {
+    owner = "Multirious";
+    repo = "zsh-helix-mode";
+    rev = "f9d72d23f5ce12da31967a6ecd15dcafa1355fa7";
+    sha256 = "sha256-wiUocowz3uL3fTAxTfG3URhOwB2RrFWEmUnW4k0ETJ4=";
+  };
+in
 {
   programs.zsh = {
     enable = true;
     shellAliases = {
-      e = "eza";
       rsync = "rsync --info=progress2";
-      yt-dlp = "yt-dlp --embed-subs";
-    };
-    ohMyZsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "direnv"
-        "vi-mode"
-        "fzf"
-      ];
-      customPkgs = with pkgs; [ spaceship-prompt ];
-      theme = "spaceship";
-    };
+      ergedox_flash = "teensy-loader-cli -mmcu=atmega32u4 -w";
+    }
+    // (
+      let
+        ndots = x: lib.concatStrings (lib.replicate x ".");
+        ndirs = x: lib.concatStrings (lib.replicate x "../");
+        alias = x: {
+          name = ndots (x + 1);
+          value = "cd ${ndirs x}";
+        };
+      in
+      builtins.listToAttrs (map alias (lib.range 1 10))
+    );
 
-    promptInit = builtins.readFile ./zshrc;
+    promptInit = ''
+      export BROWSER=firefox
+      export EDITOR=hx
+      eval $(${lib.getExe pkgs.starship} init zsh)
+      # Search history with initial parts of command filled in
+      bindkey "$terminfo[kcuu1]" history-beginning-search-backward
+      bindkey "$terminfo[kcud1]" history-beginning-search-forward
+      # CTRL left+right
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+      zstyle ':completion:*' menu select
+      zstyle ':completion:*' matcher-list ''' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+      zstyle ':completion:*:default' list-colors ''${(s.:.)LS_COLORS}
+      # source ${zsh-helix-mode}/zsh-helix-mode.plugin.zsh
+    '';
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = lib.replaceString "\n" "" ''
+        $username
+        $hostname
+        $localip
+        $shlvl
+        $singularity
+        $kubernetes
+        $directory
+        ''${custom.jj}
+        $memory_usage
+        $direnv
+        $env_var
+        $cmd_duration
+        $line_break
+        $jobs
+        $time
+        $status
+        $os
+        $netns
+        $shell
+        $character'';
+
+      # Mostly copied from https://github.com/jj-vcs/jj/wiki/Starship#alternative-prompt
+      custom.jj = {
+        command = ''
+          jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+            separate(" ",
+              change_id.shortest(4),
+              bookmarks,
+              "|",
+              concat(
+                if(conflict, "💥"),
+                if(divergent, "🚧"),
+                if(hidden, "👻"),
+                if(immutable, "🔒"),
+              ),
+              raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+              raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+                truncate_end(29, description.first_line(), "…"),
+                "(no description set)",
+              ) ++ raw_escape_sequence("\x1b[0m"),
+            )
+          '        '';
+        when = "jj --ignore-working-copy root";
+        symbol = "jj";
+      };
+    };
   };
 }
